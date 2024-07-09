cce8b67f3097ceb919125759944fef41faea30c9
Harsh Goyal <harshgoyal1331@gmail.com>
2024-07-09 17:36:04 +0530

    Update Privacy.jsx

--- src/components/sub/Privacy.jsx
+++ src/components/sub/Privacy.jsx
@@ -7,20 +7,20 @@ const Privacy = () => {
 
       <div className="flex flex-col gap-4">
         <p>
-          KagazPrints is a registered trademark of Kagaz Prints Pvt. Ltd., a
+          KaagazPrint is a registered trademark of Kaagaz Prints Pvt. Ltd., a
           company incorporated under the [Applicable Laws]. Our registered
-          office is located at [Your Address]. The domain name kagazprints.com
-          is owned and operated by Kagaz Prints Pvt. Ltd.
+          office is located at [Your Address]. The domain name kaagazprint.com
+          is owned and operated by Kaagaz Prints Pvt. Ltd.
         </p>
         <p>
           We deeply respect your privacy and the trust you place in us. Our
           Privacy Policy outlines how we collect, use, and disclose information
-          related to you
+          related to you.
         </p>
         <p>
           By accessing our website/app, you agree to abide by the terms and
           conditions of our Privacy Policy and consent to the collection,
-          storage, and utilization of information as described herein
+          storage, and utilization of information as described herein.
         </p>
         <p>
           If you do not agree with any part of our Privacy Policy, especially
@@ -31,11 +31,11 @@ const Privacy = () => {
           Our Privacy Policy is an integral part of our Terms and Conditions of
           Use and may be subject to changes without prior notice. We encourage
           you to periodically review our Privacy Policy on our website/app for
-          any updates
+          any updates.
         </p>
         <p>
           If you have any questions or need clarification regarding our Privacy
-          Policy, please don't hesitate to contact us at info@kagazprints.com.
+          Policy, please don't hesitate to contact us at info@kaagazprint.com.
           We're here to address any concerns you may have.
         </p>
       </div>
