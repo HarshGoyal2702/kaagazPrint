fcbf04893a819b0d909e494fc97fb0bd821db495
Harsh Goyal <harshgoyal1331@gmail.com>
2024-07-09 17:19:41 +0530

    Update Communication.jsx

--- src/components/sub/Communication.jsx
+++ src/components/sub/Communication.jsx
@@ -5,24 +5,27 @@ const Communication = () => {
     <div>
       <div className="container mx-auto px-4 py-4">
         <h1 className="font-sans font-bold text-3xl mb-3">
-          Communication with KagazPrints
+          Communication with Kaagazprint
         </h1>
 
         <div>
-          <p className="font-gray-600 flex flex-col gap-2">
+          <p className="text-gray-600 flex flex-col gap-2">
             If you need to correct or update any information you've provided,
             you have two options: <br />
             <div>
-              <strong>Online Correction </strong> You can easily update your
-              information directly on our website. Email Correction:
-              Alternatively, you may contact us via email at:{" "}
-              <a href="info@kagazprints.com">info@kagazprints.com</a>
-            </div>{" "}
+              <strong>Online Correction: </strong> You can easily update your
+              information directly on our website.
+            </div>
             <div>
-              to request any corrections or updates. In case you experience any
-              issues accessing our website, please don't hesitate to reach out
-              to us via email at:
-              <a href="info@kagazprints.com ">info@kagazprints.com </a>for
+              <strong>Email Correction: </strong> Alternatively, you may contact
+              us via email at:{" "}
+              <a href="mailto:info@kaagazprint.com">info@kaagazprint.com</a> to
+              request any corrections or updates.
+            </div>
+            <div>
+              In case you experience any issues accessing our website, please
+              don't hesitate to reach out to us via email at:{" "}
+              <a href="mailto:info@kaagazprint.com">info@kaagazprint.com</a> for
               assistance.
             </div>
           </p>
