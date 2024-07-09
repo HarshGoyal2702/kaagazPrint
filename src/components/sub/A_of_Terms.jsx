163be21ae8b3c44b929706fd4c062209e2622132
Harsh Goyal <harshgoyal1331@gmail.com>
2024-07-09 17:18:02 +0530

    Update A_of_Terms.jsx

--- src/components/sub/A_of_Terms.jsx
+++ src/components/sub/A_of_Terms.jsx
@@ -6,25 +6,25 @@ const A_of_Terms = () => {
       <div className="container mx-auto px-4 py-8">
         <h1 className="text-3xl font-bold mb-4">Acceptance of Terms</h1>
         <p className="text-base mb-4">
-          Thank you for using KagazPrints. These Terms of Use (the "Terms") are
+          Thank you for using Kaagazprint. These Terms of Use (the "Terms") are
           designed to inform you about your legal rights and responsibilities
-          concerning your usage of the KagazPrints website at{" "}
-          <a href="www.kagazprints.com" className="text-blue-500">
-            www.kagazprints.com
+          concerning your usage of the Kaagazprint website at{" "}
+          <a href="www.kaagazprint.com" className="text-blue-500">
+            www.kaagazprint.com
           </a>{" "}
           (the "Site") and any associated mobile or software applications
-          ("KagazPrints Platform"), including the delivery of information via
+          ("Kaagazprint Platform"), including the delivery of information via
           the website, both present and future, that link to these Terms
           (collectively, the "Services").
         </p>
         <p className="text-base mb-4">
-          These Terms apply to all existing and future KagazPrints customers. By
-          accessing the KagazPrints website (hereinafter the “Marketplace”), you
+          These Terms apply to all existing and future Kaagazprint customers. By
+          accessing the Kaagazprint website (hereinafter the “Marketplace”), you
           agree to be bound by these Terms and acknowledge that they constitute
           a legally binding agreement between you and the Company (hereinafter
           the “User Agreement”). If you do not accept these Terms or are unable
           to abide by them, you may not utilize the Services. Your use of the
-          KagazPrints Platform is undertaken at your own risk, including the
+          Kaagazprint Platform is undertaken at your own risk, including the
           risk of exposure to content that may be objectionable or
           inappropriate.
         </p>
@@ -44,8 +44,8 @@ const A_of_Terms = () => {
         <p className="text-base mb-4">
           If you require clarification regarding the Terms of Use, please feel
           free to contact us at{" "}
-          <a href="mailto:info@kagazprints.com" className="text-blue-500">
-            info@kagazprints.com
+          <a href="mailto:info@kaagazprint.com" className="text-blue-500">
+            info@kaagazprint.com
           </a>
           .
         </p>
