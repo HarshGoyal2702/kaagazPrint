506a5aa3c1453e2caed732717b81f0d6d73779c5
Harsh Goyal <harshgoyal1331@gmail.com>
2024-07-09 17:38:20 +0530

    Update T_U.jsx

--- src/components/sub/T_U.jsx
+++ src/components/sub/T_U.jsx
@@ -2,46 +2,46 @@ import React from "react";
 
 const TermsOfUse = () => {
   return (
-    <div className=" container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-4">Terms of Use</h1>
       <p className="text-base mb-4">
-          Thank you for choosing KagazPrints, provided by Kagaz Prints. Please
-          carefully review these Terms of Use before utilizing our services.
-          "KagaazPrints" is a trademark owned by Kagaz Prints, a company
-          incorporated under the [Applicable Laws] with its registered office
-          located at [Your Address]. The domain www.kagazprints.com is the
-          property of Kagaz Prints.
-        </p>
-        <p className="text-base mb-4">
-          We wish to inform you that KagazPrints has recently undergone a
-          re-branding exercise. As of [Re-branding Date], our brand name has
-          transitioned from [Previous Name] to "KagazPrints." Consequently, we
-          are in the process of updating all relevant documents, including our
-          terms & conditions and policies. KagazPrints is owned and managed by
-          Kagaz Prints. We want to clarify that we have no affiliation,
-          connection, or association with any other business entities,
-          including those with similar names or trademarks.
-        </p>
-        <p className="text-base mb-4">
-          By using KagazPrints, you agree to abide by all applicable laws and
-          regulations. It is your responsibility to ensure that your use of our
-          services complies with legal requirements. We reserve the right to
-          amend or modify these Terms of Use at any time without prior notice.
-          Any revisions will become effective immediately upon being posted on
-          our website. We encourage you to periodically review these terms for
-          any updates. If you have any questions or concerns regarding these
-          Terms of Use, please do not hesitate to contact us at [Your Contact
-          Information].
-        </p>
-        <p className="text-base mb-4">
-          Your access to and utilization of KagazPrints signifies your
-          acceptance of these Terms of Use. If you do not agree with these
-          terms, we kindly ask that you refrain from using our services.
-        </p>
-        <p className="text-base mb-4">
-          Thank you for choosing KagazPrints. We appreciate the opportunity to
-          serve you!
-        </p>
+        Thank you for choosing KaagazPrint, provided by Kagaz Prints. Please
+        carefully review these Terms of Use before utilizing our services.
+        "KaagazPrint" is a trademark owned by Kagaz Prints, a company
+        incorporated under the [Applicable Laws] with its registered office
+        located at [Your Address]. The domain www.kaagazprint.com is the
+        property of Kagaz Prints.
+      </p>
+      <p className="text-base mb-4">
+        We wish to inform you that KaagazPrint has recently undergone a
+        re-branding exercise. As of [Re-branding Date], our brand name has
+        transitioned from [Previous Name] to "KaagazPrint". Consequently, we
+        are in the process of updating all relevant documents, including our
+        terms & conditions and policies. KaagazPrint is owned and managed by
+        Kagaz Prints. We want to clarify that we have no affiliation,
+        connection, or association with any other business entities,
+        including those with similar names or trademarks.
+      </p>
+      <p className="text-base mb-4">
+        By using KaagazPrint, you agree to abide by all applicable laws and
+        regulations. It is your responsibility to ensure that your use of our
+        services complies with legal requirements. We reserve the right to
+        amend or modify these Terms of Use at any time without prior notice.
+        Any revisions will become effective immediately upon being posted on
+        our website. We encourage you to periodically review these terms for
+        any updates. If you have any questions or concerns regarding these
+        Terms of Use, please do not hesitate to contact us at [Your Contact
+        Information].
+      </p>
+      <p className="text-base mb-4">
+        Your access to and utilization of KaagazPrint signifies your
+        acceptance of these Terms of Use. If you do not agree with these
+        terms, we kindly ask that you refrain from using our services.
+      </p>
+      <p className="text-base mb-4">
+        Thank you for choosing KaagazPrint. We appreciate the opportunity to
+        serve you!
+      </p>
     </div>
   );
 };
